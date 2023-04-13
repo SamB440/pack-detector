6e23d1bb3297aedcc7998e4d551337a0226518d8
SamB440 <sam@islandearth.net>
2023-04-13 16:42:09 +0100

    1.19.4 release

--- src/main/java/com/convallyria/packdetector/mixin/PacketMixin.java
+++ src/main/java/com/convallyria/packdetector/mixin/PacketMixin.java
@@ -1,11 +1,10 @@
 package com.convallyria.packdetector.mixin;
 
-import com.convallyria.packdetector.mixin.accessor.PackStatusAccessor;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.ClientConnection;
-import net.minecraft.network.Packet;
 import net.minecraft.network.listener.PacketListener;
+import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.c2s.play.ResourcePackStatusC2SPacket;
 import net.minecraft.network.packet.s2c.play.ResourcePackSendS2CPacket;
 import net.minecraft.text.Text;
@@ -20,30 +19,27 @@ public class PacketMixin {
     @Inject(at = @At(value = "HEAD"),
             method = "handlePacket")
     private static <T extends PacketListener> void onHandlePacket(Packet<T> packet, PacketListener listener, CallbackInfo ci) {
-        if (packet instanceof ResourcePackSendS2CPacket) {
-            ResourcePackSendS2CPacket pack = (ResourcePackSendS2CPacket) packet;
+        if (packet instanceof ResourcePackSendS2CPacket pack) {
             MinecraftClient.getInstance().execute(() -> {
                 final ClientPlayerEntity player = MinecraftClient.getInstance().player;
                 if (player == null) return;
-                player.sendMessage(Text.of("Received resource pack: " + pack.getURL()), false);
-                player.sendMessage(Text.of("Hash: " + pack.getSHA1()), false);
-//                if (pack.getPrompt() != null) {
-//                    player.sendMessage(Text.literal("Prompt: " + pack.getPrompt().toString()));
-//                }
+                player.sendMessage(Text.literal("Received resource pack: " + pack.getURL()));
+                player.sendMessage(Text.literal("Hash: " + pack.getSHA1()));
+                if (pack.getPrompt() != null) {
+                    player.sendMessage(Text.literal("Prompt: " + pack.getPrompt().toString()));
+                }
             });
         }
     }
 
     @Inject(at = @At(value = "HEAD"),
-            method = "send(Lnet/minecraft/network/Packet;)V")
+            method = "send(Lnet/minecraft/network/packet/Packet;)V")
     public void onSendPacket(Packet<?> packet, CallbackInfo ci) {
-        if (packet instanceof ResourcePackStatusC2SPacket) {
-            ResourcePackStatusC2SPacket status = (ResourcePackStatusC2SPacket) packet;
-            PackStatusAccessor accessor = (PackStatusAccessor) status;
+        if (packet instanceof ResourcePackStatusC2SPacket status) {
             MinecraftClient.getInstance().execute(() -> {
                 final ClientPlayerEntity player = MinecraftClient.getInstance().player;
                 if (player == null) return;
-                player.sendMessage(Text.of("Responded with status: " + accessor.getStatus()), false);
+                player.sendMessage(Text.literal("Responded with status: " + status.getStatus()));
             });
         }
     }
